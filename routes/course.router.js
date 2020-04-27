de4e6493ec7cc644856a6a7dab138a370c34d92c
David <ddevargas@lean-tech.io>
2020-04-27 13:17:30 -0500

    Adding course routing methods

--- routes/course.router.js
+++ routes/course.router.js
@@ -7,7 +7,38 @@ router.get('/', async (req, res) => {
 	} catch (e) {
 		console.error(e)
 	}
-	
+});
+
+router.post('/', async (req, res) => {
+	try {
+		console.log(req.body)
+	} catch (e) {
+		console.error(e)
+	}	
+});
+
+router.get('/:id', async (req, res) => {
+	try {
+		console.log(req.params.id)
+	} catch (e) {
+		console.error(e)
+	}	
+});
+
+router.put('/:id', async (req, res) => {
+	try {
+		console.log(req.params.id, req.body)
+	} catch (e) {
+		console.error(e)
+	}	
+});
+
+router.delete('/:id', async (req, res) => {
+	try {
+		console.log(req.params.id)
+	} catch (e) {
+		console.error(e)
+	}	
 });
 
 module.exports = router;
