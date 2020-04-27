5fb8519864cf0ad62c5cd251565f15958e70834f
David <ddevargas@lean-tech.io>
2020-04-27 14:59:31 -0500

    db connection settings

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-
 const express = require('express');
 const courses = require('./api/course.api');
 const home = require('./api/home.api');
+const mongoose = require('mongoose');
+const config = require('config');
+
+const DATABASE_URL = config.get('db.url');
+
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+		.then(() => console.log('Connected to MongoDB...'))
+		.catch(err => console.error('Could not connect to MongoDB...', err));
 
 const app = express();
 app.use(express.json());
