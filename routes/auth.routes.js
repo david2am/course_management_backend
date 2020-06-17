a388fe5bc896a76d4b42b44fe30091ec5d4f71b9
David <ddevargas@lean-tech.io>
2020-06-17 10:52:29 -0500

    setting OAuth routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,1 +1,17 @@
 const router = require('express').Router();
+
+router.get('/login', (req, res) => {
+    res.render('login')
+});
+
+router.get('/logout', (req, res) => {
+    // handle with passport
+    res.send('logging out')
+});
+
+router.get('/google', (req, res) => {
+    // handle with passport
+    res.send('logging with google')
+});
+
+module.exports = router;
--- startup/routes.js
+++ startup/routes.js
@@ -2,18 +2,20 @@ const express = require('express');
 
 const error = require('../middleware/error.middleware')
 
-const home = require('../routes/home.routes');
-const authors = require('../routes/author.routes');
-const courses = require('../routes/course.routes');
-const users = require('../routes/users.routes');
+const homeRoutes = require('../routes/home.routes');
+const authorRoutes = require('../routes/author.routes');
+const courseRoutes = require('../routes/course.routes');
+const userRoutes = require('../routes/users.routes');
+const authRoutes = require('../routes/auth.routes');
 
 
 function routes(app) {
     app.use(express.json());
-    app.use('/', home);
-    app.use('/api/authors', authors);
-    app.use('/api/courses', courses);
-    app.use('/api/users', users);
+    app.use('/', homeRoutes);
+    app.use('/api/authors', authorRoutes);
+    app.use('/api/courses', courseRoutes);
+    app.use('/api/users', userRoutes);
+    app.use('/auth', authRoutes)
 
     app.use(error)
 }
