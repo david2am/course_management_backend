b286acde6665399d4d8abb77c4fd30f70f2c9c42
David <ddevargas@lean-tech.io>
2020-06-17 09:50:28 -0500

    router refactor

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,7 @@
 const auth = require('../middleware/auth.middleware');
 const { me, register, login } = require('../controllers/users.controller');
 
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 
 router.get('/me', [auth], me);
 router.post('/register', register);
